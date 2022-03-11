20b38883c718dfb83ce736f7052afb6d175c24d8
akashudayan <akash.udayan@trenser.com>
2022-03-11 16:12:40 +0530

    .

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -4,6 +4,7 @@ scanner(
         serverUrl: process.argv[2],
 
         options: {
+            "sonar.organization":"my-app",
             "sonar.sources": "./src",
             "sonar.exclusions": "**/*.test.tsx",
             "sonar.tests": "./src",
