1a6c895c0b27d69e75641d540989f5a498288490
akashudayan <akash.udayan@trenser.com>
2022-03-11 15:56:12 +0530

    sonar-scanner implimentation

---
+++ sonarqube-scanner.js
@@ -0,0 +1,16 @@
+const scanner = require('sonarqube-scanner');
+scanner(
+    {
+        serverUrl: process.argv[2],
+
+        options: {
+            "sonar.sources": "./src",
+            "sonar.exclusions": "**/*.test.tsx",
+            "sonar.tests": "./src",
+            "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts",
+            "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+            "sonar.testExecutionReportPaths": "test-report.xml"
+        },
+    },
+    () => process.exit()
+);
